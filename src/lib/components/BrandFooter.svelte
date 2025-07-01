826d12e332e4e86dd59b53ae6199877698cc40d6
JackDolbs <hi@jackdolbs.me>
2025-07-01 11:12:40 +0100

    text size change

--- src/lib/components/BrandFooter.svelte
+++ src/lib/components/BrandFooter.svelte
@@ -5,7 +5,7 @@
 <section class="w-full pb-10 md:pb-20 bg-neutral-50">
   <div class="max-w-7xl mx-auto px-4">
     <div class="text-center">
-      <h2 class="text-8xl sm:text-9xl md:text-[12rem] lg:text-[18rem] font-orbitron font-bold tracking-wider bg-gradient-to-r from-neutral-800 to-neutral-900 bg-clip-text text-transparent transition-opacity duration-300">
+      <h2 class="text-7xl sm:text-9xl md:text-[12rem] lg:text-[18rem] font-orbitron font-bold tracking-wider bg-gradient-to-r from-neutral-800 to-neutral-900 bg-clip-text text-transparent transition-opacity duration-300">
         AURIEL
       </h2>
     </div>
